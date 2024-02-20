fd5e71f129152e131501d58a31412a2f846d95b7
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-02-20 21:48:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Player from "./components/Player.js";
+import Player from './components/Player.js';
+import TimeChallenge from './components/TimeChallenge.js';
 
 function App() {
+
+  
   return (
     <>
       <header>
         <h1>
-          The <em>Almost</em> Final Countdown
+          The <em>Internal</em> Clock
         </h1>
-        <p>Stop the timer once you estimate that time is (almost) up</p>
+        <p>Stop the timer once your internal clock estimates that the time is up.</p>
       </header>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimeChallenge title="Easy" targetTime={1} />
+        <TimeChallenge title="Normal" targetTime={5} />
+        <TimeChallenge title="Getting Tough" targetTime={15} />
+        <TimeChallenge title="Pros Only" targetTime={25} />
+      </div>
     </>
   );
 }
